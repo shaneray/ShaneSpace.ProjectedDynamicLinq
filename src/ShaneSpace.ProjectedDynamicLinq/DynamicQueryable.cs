b255b586e61d3e54e50014d96fddd6140b4342f3
Shane Ray <Shane.Ray@lingo.com>
2019-07-30 09:03:10 -0400

    Added ability to register MappingConfigurationProvider and added extension method overloads wihout MappingConfigurationProvider parameter.

--- src/ShaneSpace.ProjectedDynamicLinq/DynamicQueryable.cs
+++ src/ShaneSpace.ProjectedDynamicLinq/DynamicQueryable.cs
@@ -14,55 +14,106 @@ namespace ShaneSpace.ProjectedDynamicLinq
 {
     public static class DynamicQueryable
     {
+        public static IConfigurationProvider MappingConfigurationProvider { get; set; }
+
+        public static IConfigurationProvider UseProjectedDynamicLinq(this IConfigurationProvider configuration)
+        {
+            MappingConfigurationProvider = configuration;
+            return configuration;
+        }
+
+        public static IQueryable<T> Where<T>(this IQueryable<T> source, string predicate, params object[] values)
+        {
+            return (IQueryable<T>)Where<T>((IQueryable)source, null, predicate, values);
+        }
+
+        public static IQueryable<TSource> Where<TSource, TDestination>(this IQueryable<TSource> source, string predicate, params object[] values)
+        {
+            return (IQueryable<TSource>)Where<TDestination>(source, null, predicate, values);
+        }
+
         public static IQueryable<T> Where<T>(this IQueryable<T> source, IConfigurationProvider mapperConfigurationProvider, string predicate, params object[] values)
         {
             return (IQueryable<T>)Where<T>((IQueryable)source, mapperConfigurationProvider, predicate, values);
         }
+
         public static IQueryable<TSource> Where<TSource, TDestination>(this IQueryable<TSource> source, IConfigurationProvider mapperConfigurationProvider, string predicate, params object[] values)
         {
-            return (IQueryable<TSource>)Where<TDestination>((IQueryable)source, mapperConfigurationProvider, predicate, values);
+            return (IQueryable<TSource>)Where<TDestination>(source, mapperConfigurationProvider, predicate, values);
         }
+
         private static IQueryable Where<TDestination>(this IQueryable source, IConfigurationProvider mapperConfigurationProvider, string predicate, params object[] values)
         {
-            if (source == null) throw new ArgumentNullException("source");
-            if (predicate == null) throw new ArgumentNullException("predicate");
-            LambdaExpression lambda = DynamicExpression.ParseLambda<TDestination>(mapperConfigurationProvider, source.ElementType,  typeof(bool), predicate, values);
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            var configurationProvider = GetMappingConfigurationProvider(mapperConfigurationProvider);
+
+            var lambda = DynamicExpression.ParseLambda<TDestination>(configurationProvider, source.ElementType, typeof(bool), predicate, values);
             return source.Provider.CreateQuery(
                 Expression.Call(
                     typeof(Queryable), "Where",
-                    new Type[] { source.ElementType },
+                    new[] { source.ElementType },
                     source.Expression, Expression.Quote(lambda)));
+        }
+
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering, params object[] values)
+        {
+            return (IQueryable<T>)OrderBy<T>((IQueryable)source, null, ordering, values);
+        }
+
+        public static IQueryable<TSource> OrderBy<TSource, TDestination>(this IQueryable<TSource> source, string ordering, params object[] values)
+        {
+            return (IQueryable<TSource>)OrderBy<TDestination>(source, null, ordering, values);
         }
 
         public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, IConfigurationProvider mapperConfigurationProvider, string ordering, params object[] values)
         {
             return (IQueryable<T>)OrderBy<T>((IQueryable)source, mapperConfigurationProvider, ordering, values);
         }
+
         public static IQueryable<TSource> OrderBy<TSource, TDestination>(this IQueryable<TSource> source, IConfigurationProvider mapperConfigurationProvider, string ordering, params object[] values)
         {
-            return (IQueryable<TSource>)OrderBy<TDestination>((IQueryable)source, mapperConfigurationProvider, ordering, values);
+            return (IQueryable<TSource>)OrderBy<TDestination>(source, mapperConfigurationProvider, ordering, values);
         }
-        public static IQueryable OrderBy<TDestDest>(this IQueryable source, IConfigurationProvider mapperConfigurationProvider, string ordering, params object[] values)
+
+        private static IQueryable OrderBy<TDestination>(this IQueryable source, IConfigurationProvider mapperConfigurationProvider, string ordering, params object[] values)
         {
-            if (source == null) throw new ArgumentNullException("source");
-            if (ordering == null) throw new ArgumentNullException("ordering");
-            ParameterExpression[] parameters = new ParameterExpression[] {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (ordering == null) throw new ArgumentNullException(nameof(ordering));
+            var configurationProvider = GetMappingConfigurationProvider(mapperConfigurationProvider);
+
+
+            ParameterExpression[] parameters = {
                 Expression.Parameter(source.ElementType, "") };
-            ExpressionParser<TDestDest> parser = new ExpressionParser<TDestDest>(parameters, ordering, values, mapperConfigurationProvider);
-            IEnumerable<DynamicOrdering> orderings = parser.ParseOrdering();
-            Expression queryExpr = source.Expression;
-            string methodAsc = "OrderBy";
-            string methodDesc = "OrderByDescending";
-            foreach (DynamicOrdering o in orderings)
+            var parser = new ExpressionParser<TDestination>(parameters, ordering, values, configurationProvider);
+            var orderings = parser.ParseOrdering();
+            var queryExpr = source.Expression;
+            var methodAsc = "OrderBy";
+            var methodDesc = "OrderByDescending";
+            foreach (var o in orderings)
             {
                 queryExpr = Expression.Call(
                     typeof(Queryable), o.Ascending ? methodAsc : methodDesc,
-                    new Type[] { source.ElementType, o.Selector.Type },
+                    new[] { source.ElementType, o.Selector.Type },
                     queryExpr, Expression.Quote(Expression.Lambda(o.Selector, parameters)));
                 methodAsc = "ThenBy";
                 methodDesc = "ThenByDescending";
             }
             return source.Provider.CreateQuery(queryExpr);
+        }
+
+        private static IConfigurationProvider GetMappingConfigurationProvider(IConfigurationProvider configurationProvider = null)
+        {
+            var output = configurationProvider
+                         ?? MappingConfigurationProvider
+                         ?? Mapper.Configuration;
+
+            if (output == null)
+            {
+                throw new ArgumentNullException($"Please call {nameof(DynamicQueryable)}.{nameof(MappingConfigurationProvider)} prior to calling methods without {nameof(IConfigurationProvider)} parameter or use overloads with {nameof(IConfigurationProvider)} parameters.");
+            }
+
+            return output;
         }
     }
 
@@ -565,12 +616,12 @@ namespace ShaneSpace.ProjectedDynamicLinq
         int textLen;
         char ch;
         Token token;
-        IConfigurationProvider mapperConfiguration;
+        readonly IConfigurationProvider _mapperConfiguration;
 
         public ExpressionParser(ParameterExpression[] parameters, string expression, object[] values, IConfigurationProvider mapperConfigurationProvider)
         {
             if (expression == null) throw new ArgumentNullException("expression");
-            mapperConfiguration = mapperConfigurationProvider;
+            _mapperConfiguration = mapperConfigurationProvider;
             if (keywords == null) keywords = CreateKeywords();
             symbols = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
             literals = new Dictionary<Expression, string>();
@@ -1219,7 +1270,7 @@ namespace ShaneSpace.ProjectedDynamicLinq
             MemberInfo member;
             // Check for Automapper map
             var destinationType = typeof(TDest);
-            var mappingConfig = mapperConfiguration.FindTypeMapFor(type, destinationType);
+            var mappingConfig = _mapperConfiguration.FindTypeMapFor(type, destinationType);
             if (mappingConfig != null)
             {
                 var propMaps = mappingConfig.GetPropertyMaps().ToList();
